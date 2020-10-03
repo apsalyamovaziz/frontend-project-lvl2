2334f479a6be8d1127ff389f042d67645c7cc858
Апсалямов Азиз <apsalamovaziz@MacBook-Air-Apsalamov.local>
2020-10-03 18:59:08 +0600

    step2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,7 @@ const program = new commander.Command();
 program
     .version('0.0.1', '-v, --version', 'output the current version')
     .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format')
+    .arguments('<filepath1> <filepath2>')
 
 program.parse(process.argv);
