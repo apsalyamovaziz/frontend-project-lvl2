bfcc13309518541df7454f92a387aae4ea5477a9
Апсалямов Азиз <apsalamovaziz@MacBook-Air-Apsalamov.local>
2020-10-03 18:19:18 +0600

    step1

--- Makefile
+++ Makefile
@@ -6,3 +6,4 @@ publish:
 
 lint:
 	npx eslint .
+
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,10 @@
 #!/usr/bin/env node
+
+import commander from 'commander';
+const program = new commander.Command();
+
+program
+    .version('0.0.1', '-v, --version', 'output the current version')
+    .description('Compares two configuration files and shows a difference.')
+
+program.parse(process.argv);
