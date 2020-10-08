90b2b9539a8239f4a0c88857a663aaa75f701cd2
Aziz Apsalyamov <apsalamovaziz@MacBook-Air-Apsalamov.local>
2020-10-09 02:58:31 +0600

    step3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
 
 import commander from 'commander';
+import diff from '../src/index.js';
+
+
 const program = new commander.Command();
 
 program
-    .version('0.0.1', '-v, --version', 'output the current version')
-    .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format [type]', 'output format')
-    .arguments('<filepath1> <filepath2>')
+  .version('0.0.1', '-v, --version', 'output the current version')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(diff(filepath1, filepath2));
+  });
 
 program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
+import pkg from 'lodash';
+const { has } = pkg;
+import fs from 'fs';
 
 
+const diff = (file1, file2) => {
+    const result = {};
+
+    const readData1 = fs.readFileSync(file1)
+    const readData2 = fs.readFileSync(file2);
+
+    const data1 = JSON.parse(readData1);
+    const data2 = JSON.parse(readData2);
+  
+    Object.keys(data2).forEach((key) => {
+      if (has(data1, key)) {
+        if (data2[key] === data1[key]) {
+          result[key] = data1[key];
+        } else {
+          result[`- ${key}`] = data1[key];
+          result[`+ ${key}`] = data2[key];
+        }
+      } else {
+        result[`+ ${key}`] = data2[key];
+      }
+    });
+  
+    Object.keys(data1).forEach((key) => {
+      if (!has(data2, key)) {
+        result[`- ${key}`] = data1[key];
+      }
+    });
+  
+    return result;
+  };
+  
+  export default diff;
